fb0d69f9491a3ec5f983e58416286c17445546bf
jonbingkim <jonkim@Jonathans-MBP.attlocal.net>
2023-10-06 13:10:20 -0700

    merging with dev

--- client/components/userLogin.js
+++ client/components/userLogin.js
@@ -24,9 +24,9 @@ return (
 <h1>Login Here</h1>
 <form onSubmit={handlelogin}>
   <label htmlFor='username'><strong>Username</strong></label><br></br>
-  <input type='text' value={user.username}></input><br></br>
+  <input type='text' ></input><br></br>
   <label htmlFor='password'><strong>Password</strong></label><br></br>
-  <input type='text' value={user.password}></input><br></br>
+  <input type='text'></input><br></br>
   <button type='submit' >Sign in</button>
   <button type="button" onClick={toggle}>sign up</button>
 </form>
