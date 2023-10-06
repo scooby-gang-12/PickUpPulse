138ff4e9766021a7ee60ef59551dfcad3e502ea3
John Arroyo II <jtarroyo2@gmail.com>
2023-10-06 15:45:17 -0600

    adjusted gameSchema, added address and sport, renamed gameType to gameName, sport will function as gameType moving forward

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,21 +4,33 @@ const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: {type: String, required: true, index: { unique: true} },
-  password: {type: String, required: true, bcrypt: true},
+  password: {type: String, required: true},
   fullName: {type: String, required: true},
-  userLocation: {type: String, required: true},
-  favoriteSports: {type: [String], require: true}
+  favoriteSports: {type: [String], require: true},
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  }
 });
 
-UserSchema.pre('save', (next) => {
-  if (!this.ismodified('password')) return next();
+UserSchema.pre('save', function(next){
+  if (!this.isModified('password')) return next();
   bcrypt.hash(this.password, process.env.SALT_FACTOR, (err, hash) => {
     if (err) return next(err);
     this.password = hash;
     return next();
   })
-})
+});
 
-const User = mongoose.model('user', UserSchema);
+UserSchema.index({ location: '2dsphere'});
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
