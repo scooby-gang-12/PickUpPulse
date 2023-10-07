291dc150b4e5d24c2b065310ebe8bd4992dbad58
Just Bobby <robertstipp17@gmail.com>
2023-10-07 15:09:13 -0700

    Added gameSlice extraBuilder cases

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -7,7 +7,7 @@ const UserSchema = new Schema({
   username: {type: String, required: true, index: { unique: true} },
   password: {type: String, required: true},
   fullName: {type: String, required: true},
-  favoriteSports: {type: [String], require: true},
+  favoriteSports: {type: [String], required: true},
   location: {
     type: {
       type: String,
