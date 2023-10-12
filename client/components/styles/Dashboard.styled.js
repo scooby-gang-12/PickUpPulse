4f0760dc7e9441d91ddad65db90360773e833294
jonbingkim <jonkimtw@gmail.com>
2023-10-11 19:20:33 -0700

    pushing changes for merging

--- client/components/styles/Dashboard.styled.js
+++ client/components/styles/Dashboard.styled.js
@@ -7,6 +7,7 @@ grid-template-columns: repeat(2, 50%);
 grid-column-gap: 10px;
 justify-items: center;
 text-align: center;
+margin-top: 40px;
 `
 
 export const StyledLeftSide = styled.div`
@@ -15,4 +16,19 @@ grid-template-rows: repeat(2, 50%);
 grid-row-gap: 10px;
 justify-items: center;
 text-align: center;
+`
+
+export const StyledAllGames = styled.div`
+justify-content: center;
+justify-items: center;
+text-align: center;
+`
+
+export const StyledRenderedGames = styled.div`
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  overflow-y: auto;
+  width: 300px;
+  max-height: 300px;
 `
